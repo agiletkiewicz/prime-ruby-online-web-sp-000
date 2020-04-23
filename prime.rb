c426a17991be4240860df3ff89404c1d6c27e938
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-23 21:40:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 
 def prime?(number)
   range = (1..(number - 1)).to_a
-  range.any? do |integer|
+  if range.any? do |integer|
     (number % integer == 0) 
+    return true 
+  else 
+    return false 
+  end
   end
 end
